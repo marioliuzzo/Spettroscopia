7a77412995b079fb1ff802a11ccde60f34c2beb2
unknown <l.pierfederici@studenti.unipi.it>
2022-11-10 23:01:48 +0100

    Create total_counts.py

---
+++ total_counts.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+PATH = 'C:/Users/Lorenzo/Desktop/Lab/Spettroscopia/spettri'#percorso dei file .txt
+NOME_SPETTRO = 'Am241_1.txt' #modificare con il nome del file
+PATH = os.path.join(PATH, NOME_SPETTRO)
+
+counts = np.loadtxt(PATH, skiprows=12, max_rows=2048, unpack = True) #salta i commenti ed acquisice i conteggi dei canali 0-2047
+channels = np.array([i for i in range(0, 2048)], dtype = float) #numero di canali
+
+plt.plot(channels, counts, marker = 'o')
+NOME_SPETTRO = NOME_SPETTRO.replace('_1.txt', '')
+plt.title('Channels vs counts' + ' ' + NOME_SPETTRO)
+plt.xlabel('Channels')
+plt.ylabel('Counts')
+plt.minorticks_on()
+plt.show()
